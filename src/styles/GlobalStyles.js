3fe384ddd6646caae23adc8ec65cd071eb9310b5
seocylucky <seocylucky@github.com>
2022-10-07 02:17:55 +0900

    chore: 프로젝트 세팅

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+    *{
+         box-sizing:boerder-box;
+     }
+`  
---
+++ src/styles/colors.js
@@ -0,0 +1,18 @@
+const colors = {
+    black_100: "#303132",
+    black_90: "#424344",
+    black_80: "#535455",
+    black_70: "#666667",
+    black_60: "#787879",
+    black_50: "#89898A",
+    black_40: "#9B9C9C",
+    black_30: "#ACADAD",
+    black_20: "#C0C0C0",
+    black_10: "#D2D2D2",
+    white_100: "#FFFFFF",
+    bgColor: "#F5F5F7",
+    pointColor_1: "#9E3DFF",
+    pointColor_2: "3840FF",
+};
+
+export default colors;
