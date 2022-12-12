5badcb848d475ff8dc8f20d5cc4465d6bc755e1b
seocylucky <seocylucky@github.com>
2022-12-12 23:55:06 +0900

    feat: 라우터 수정

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,19 @@ import LoginPage from './pages/Login/LoginPage';
 import SignupPage from './pages/Login/SignupPage';
 import FirstSelect from './pages/SelectPage/FirstSelect';
 import SecondSelct from './pages/SelectPage/SecondSelct';
-import QuesitonPage from './pages/SelectPage/QuesitonPage';
 import Mypage from './pages/Mypage/Mypage';
 import RecordView from './components/Accodian/Recorder/Recorder';
 import LoginNav from './components/TopNav/LoginNav/index';
-import Accodian from './components/Accodian/accodian';
+import Accodian from './components/Accodian/accodians/accodian';
+import Random from './pages/Random';
+import QuesitonPage from './pages/SelectPage/QuestionPage/QuesitonPage';
+import FEQuesiton from './pages/SelectPage/QuestionPage/FEQuestion';
+import BEQuesiton from './pages/SelectPage/QuestionPage/BEQuestion';
+import IOSQuesiton from './pages/SelectPage/QuestionPage/IOSQuestion';
+import AndroidQuesiton from './pages/SelectPage/QuestionPage/AndroidQuestion';
+import UIUXQuesiton from './pages/SelectPage/QuestionPage/UIUXQuestion';
+import PMQuesiton from './pages/SelectPage/QuestionPage/PMQuestion';
+import SearchPage from './pages/Search/SearchPage';
 
 
 function App() {
@@ -25,8 +33,16 @@ function App() {
           <Route exact={true} path="/signup" element={<SignupPage />}></Route>
           <Route exact={true} path="/select" element={<FirstSelect />}></Route>
           <Route exact={true} path="/select/role" element={<SecondSelct />}></Route>
-          <Route exact={true} path="/question" element={<QuesitonPage />}></Route>
+          <Route exact={true} path="/question/common" element={<QuesitonPage />}></Route>
+          <Route exact={true} path="/question/fe" element={<FEQuesiton />}></Route>
+          <Route exact={true} path="/question/be" element={<BEQuesiton />}></Route>
+          <Route exact={true} path="/question/ios" element={<IOSQuesiton />}></Route>
+          <Route exact={true} path="/question/android" element={<AndroidQuesiton />}></Route>
+          <Route exact={true} path="/question/uiux" element={<UIUXQuesiton />}></Route>
+          <Route exact={true} path="/question/pm" element={<PMQuesiton />}></Route>
           <Route exact={true} path="/mypage" element={<Mypage />}></Route>
+          <Route exact={true} path="/random" element={<Random />}></Route>
+          <Route exact={true} path="/search" element={<SearchPage />}></Route>
         </Routes>
         <Footer />
       </MediaDiv>
